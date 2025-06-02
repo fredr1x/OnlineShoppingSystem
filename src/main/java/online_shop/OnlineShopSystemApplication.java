ee66e39ccba402d62e3f61c7a72bd2d37f89a744
ydt <230103074@gmail.com>
2025-06-02 15:34:50 +0500

    first commit

---
+++ src/main/java/online_shop/OnlineShopSystemApplication.java
@@ -0,0 +1,13 @@
+package online_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online_shop/OnlineShopSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package online_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
