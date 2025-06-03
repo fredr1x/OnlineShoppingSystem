e3961b56b9b24c9c56719554ad70432ccfd6533e
ydt <230103074@gmail.com>
2025-06-03 18:51:25 +0500

    NEW: add new entity

--- src/main/java/online_shop/entity/WishList.java
+++ src/main/java/online_shop/entity/WishList.java
@@ -1,26 +1,26 @@
 package online_shop.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "wish_list",
-        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "product_id"}))
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "wish_list")
 public class WishList {
 
     @Id
-    @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
+
+    @OneToMany(mappedBy = "wishList", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WishListItem> items = new ArrayList<>();
 }
